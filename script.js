005fe58077994556902f7fc1d89b998154bdd4a1
qollage <65553813+qollage@users.noreply.github.com>
2020-08-05 20:55:49 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".img").hover(function () {
+    $(this).animate(
+      {
+        "font-size": "20px",
+      },
+      369
+    );
+  });
+  $(".seanronne").fadeIn(2020.118);
+});
