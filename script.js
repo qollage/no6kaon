9cb61eaeaec80c370afd1919f2d6b7fdcabf26b0
qollage <65553813+qollage@users.noreply.github.com>
2020-08-05 23:36:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 $(function () {
-  $(".img").hover(function () {
-    $(this).animate(
-      {
-        "font-size": "20px",
-      },
-      369
-    );
-  });
   $(".seanronne").fadeIn(2020.118);
 });
