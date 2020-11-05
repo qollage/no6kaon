66ee0b4c052594088b2a046126d058b45d0282e6
qollage <65553813+qollage@users.noreply.github.com>
2020-11-05 21:13:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-$(function(){$(".seanronne").fadeIn(2020.118);$(".scroll-btn").click(function(){$("html, body").animate({scrollTop:0,},369);});});
+$(function(){$(".scroll-btn").click(function(){$("html, body").animate({scrollTop:0,},369);});});
